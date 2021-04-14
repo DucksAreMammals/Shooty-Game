3dfa076a6f4dea01ab56621c296d8eb84b5fff44
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2021-04-14 10:46:28 -0500

    Thing is halfway working
    The player can move and shoot but the bullets aren't interacting with the enemies.

---
+++ Shooty Game/Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+[SerializeField] public float bulletSpeed = 10f;
+
+void Setup() {
+
+}
+// void OnTriggerEnter2D(Collider2D col) {
+//         Destroy(gameObject);
+// }
+}
---
+++ Shooty Game/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+[SerializeField] public float speed = 5f;
+[SerializeField] public float minPos = -8.5f;
+[SerializeField] public float maxPos = 8.5f;
+
+[SerializeField] public GameObject bullet;
+[SerializeField] public float timeBetweenBullets = 0.5f;
+[SerializeField] public float bulletSpawnYOffset = -0.3f;
+
+private float timeOfLastBullet = 0f;
+
+void Update()
+{
+        float translation = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        transform.Translate(translation, 0, 0);
+
+        if (transform.position.x < minPos) {
+                transform.position = new Vector3(minPos, transform.position.y, 0);
+        } else if (transform.position.x > maxPos) {
+                transform.position = new Vector3(maxPos, transform.position.y, 0);
+        }
+
+        if (Input.GetKey("space") && canShoot()) {
+                shoot();
+        }
+}
+
+private bool canShoot() {
+        return Time.time - timeOfLastBullet > timeBetweenBullets;
+}
+
+private void shoot() {
+        timeOfLastBullet = Time.time;
+        Vector3 bulletPosition = new Vector3(transform.position.x, transform.position.y + bulletSpawnYOffset, 0);
+        Instantiate(bullet, bulletPosition, Quaternion.identity);
+}
+}
