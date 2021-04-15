cf8026af52e58f205d161a661eb467437153d503
DucksAreMammals <bjorndlucas@gmail.com>
2021-04-14 22:37:20 -0500

    Fixed bullets
    I got the bullets to disappear when in a trigger.

--- Shooty Game/Assets/Scripts/Bullet.cs
+++ Shooty Game/Assets/Scripts/Bullet.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-[SerializeField] public float bulletSpeed = 10f;
+  [SerializeField] public float bulletSpeed = 10f;
 
-void Setup() {
+  void Setup() {
 
-}
-// void OnTriggerEnter2D(Collider2D col) {
-//         Destroy(gameObject);
-// }
+  }
+
+  void Update() {
+    transform.Translate(0, bulletSpeed * Time.deltaTime, 0);
+  }
+
+  void OnTriggerEnter2D(Collider2D col) {
+    Debug.Log("done");
+    Destroy(gameObject);
+  }
 }
--- Shooty Game/Assets/Scripts/Player.cs
+++ Shooty Game/Assets/Scripts/Player.cs
@@ -4,40 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-[SerializeField] public float speed = 5f;
-[SerializeField] public float minPos = -8.5f;
-[SerializeField] public float maxPos = 8.5f;
+  [SerializeField] public float speed = 5f;
+  [SerializeField] public float minPos = -8.5f;
+  [SerializeField] public float maxPos = 8.5f;
 
-[SerializeField] public GameObject bullet;
-[SerializeField] public float timeBetweenBullets = 0.5f;
-[SerializeField] public float bulletSpawnYOffset = -0.3f;
+  [SerializeField] public GameObject bullet;
+  [SerializeField] public float timeBetweenBullets = 0.5f;
+  [SerializeField] public float bulletSpawnYOffset = -0.3f;
 
-private float timeOfLastBullet = 0f;
+  private float timeOfLastBullet = 0f;
 
-void Update()
-{
-        float translation = Input.GetAxis("Horizontal") * speed;
-        translation *= Time.deltaTime;
-        transform.Translate(translation, 0, 0);
+  void Update()
+  {
+    float translation = Input.GetAxis("Horizontal") * speed;
+    translation *= Time.deltaTime;
+    transform.Translate(translation, 0, 0);
 
-        if (transform.position.x < minPos) {
-                transform.position = new Vector3(minPos, transform.position.y, 0);
-        } else if (transform.position.x > maxPos) {
-                transform.position = new Vector3(maxPos, transform.position.y, 0);
-        }
+    if (transform.position.x < minPos) {
+      transform.position = new Vector3(minPos, transform.position.y, 0);
+    } else if (transform.position.x > maxPos) {
+      transform.position = new Vector3(maxPos, transform.position.y, 0);
+    }
 
-        if (Input.GetKey("space") && canShoot()) {
-                shoot();
-        }
-}
+    if (Input.GetKey("space") && canShoot()) {
+      shoot();
+    }
+  }
 
-private bool canShoot() {
-        return Time.time - timeOfLastBullet > timeBetweenBullets;
-}
+  private bool canShoot() {
+    return Time.time - timeOfLastBullet > timeBetweenBullets;
+  }
 
-private void shoot() {
-        timeOfLastBullet = Time.time;
-        Vector3 bulletPosition = new Vector3(transform.position.x, transform.position.y + bulletSpawnYOffset, 0);
-        Instantiate(bullet, bulletPosition, Quaternion.identity);
-}
+  private void shoot() {
+    timeOfLastBullet = Time.time;
+    Vector3 bulletPosition = new Vector3(transform.position.x, transform.position.y + bulletSpawnYOffset, 0);
+    Instantiate(bullet, bulletPosition, Quaternion.identity);
+  }
 }
