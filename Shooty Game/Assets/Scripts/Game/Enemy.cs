4b44d8d38571bbc554806e3951ef6e5413304b25
DucksAreMammals <bjorndlucas@gmail.com>
2021-04-19 22:48:55 -0500

    Added enemy shooting
    The enemies now shoot potatoes

--- Shooty Game/Assets/Scripts/Game/Enemy.cs
+++ Shooty Game/Assets/Scripts/Game/Enemy.cs
@@ -6,9 +6,26 @@ public class Enemy : MonoBehaviour
 {
   [SerializeField] public int hp = 1;
   [SerializeField] public int score = 1;
+  [SerializeField] public float bulletSpawnOffsetY = -0.7f;
+
+  [SerializeField] public GameObject bullet;
+  [SerializeField] public float minTimeBetweenBullets = 0.5f;
+  [SerializeField] public float maxTimeBetweenBullets = 2f;
+
+  private float timeOfLastBullet = -100f;
+  [SerializeField] public float timeBetweenBullets;
+
+  void Start() {
+    timeBetweenBullets = Random.Range(minTimeBetweenBullets, maxTimeBetweenBullets);
+  }
 
   void Update() {
     transform.Translate(0, -5 * Time.deltaTime, 0);
+
+    if (Time.time - timeOfLastBullet > timeBetweenBullets) {
+      shoot();
+      timeOfLastBullet = Time.time;
+    }
   }
 
   public void Damage(int damage) {
@@ -23,5 +40,11 @@ public class Enemy : MonoBehaviour
     GameObject.Find("ScoreCounter").GetComponent<ScoreCounter>().AddPoints(score);
 
     Destroy(gameObject);
+  }
+
+  void shoot() {
+    Vector3 pos = new Vector3(transform.position.x, transform.position.y + bulletSpawnOffsetY, transform.position.z);
+
+    Instantiate(bullet, pos, Quaternion.identity);
   }
 }
--- Shooty Game/Assets/Scripts/Game/EnemyBullet.cs
+++ Shooty Game/Assets/Scripts/Game/EnemyBullet.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class EnemyBullet : MonoBehaviour
 {
   [SerializeField] public float bulletSpeed = 10f;
   [SerializeField] public int hp = 1;
 
   void Update() {
-    transform.Translate(0, bulletSpeed * Time.deltaTime, 0);
+    transform.Translate(0, -bulletSpeed * Time.deltaTime, 0);
   }
 
   void OnTriggerEnter2D(Collider2D other) {
     try {
-      Enemy enemy = other.gameObject.GetComponent<Enemy>();
-      enemy.Damage(hp);
+      Player player = other.gameObject.GetComponent<Player>();
+      player.Damage(hp);
     } catch {}
 
     Destroy(gameObject);
